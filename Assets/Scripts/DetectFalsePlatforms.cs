dcf2c46c5fe3a408f5afb1643c3a5e93bdca4a38
CodeNinjasSJ <57017893+CodeNinjasSJ@users.noreply.github.com>
2021-03-25 17:50:10 -0600

    send raycast that is down a little from position.
    checking raycasts and returning layer 8 debug working.

--- Assets/Scripts/DetectFalsePlatforms.cs
+++ Assets/Scripts/DetectFalsePlatforms.cs
@@ -6,54 +6,19 @@ using UnityEngine;
 
 public class DetectFalsePlatforms : MonoBehaviour
 {
-    public bool didRayHit;
-    private bool CastRays = true;
+    // private bool RayCast = Physics.Raycast( , Vector3.forward, 10f);
 
     void Update()
     {
-        // didRayHit = Physics.Raycast(transform.postition, Vector3.forward, 10f);
-        // Debug.DrawRay(transform.position, Vector3.forward, Color.red, 10f);
-
-        if(CastRays)
+        if (Physics.Raycast(transform.position, Vector3.down, 300f, 1 << LayerMask.NameToLayer("RayLayer")))
         {
-            if(Physics.Raycast(transform.position, Vector3.forward, 10f))
-            {
-                if(transform.gameObject.layer == 8)
-                {
-                    Debug.Log("WATCH OUT!!!");
-                } else 
-                {
-                    Debug.Log("you SHOULD be okay...");
-                }
-            }
+            Debug.Log("I hope this works lol");
         }
+
     }
-    
-    // public bool hit;
-    // public int layerCatch;
-    // void Update()
-    // {
-    //     hit = Physics.Raycast(transform.position, transform.position, .15f);
-    //     layerCatch = FindGameObjectsWithLayer(8);
 
-    //     if(layerCatch == 8)
-    // }
-
-    // GameObject[] FindGameObjectsWithLayer (int layer) 
-    // { 
-    //     GameObject[] goArray = FindObjectsOfType(typeof(GameObject)) as GameObject[]; 
-    //     List goList = new List(); 
-    //     for (int i = 0; i < goArray.Length; i++) 
-    //     { 
-    //         if (goArray[i].layer == layer) 
-    //         { 
-    //             goList.Add(goArray[i]); 
-    //         } 
-    //     } 
-    //     if (goList.Count == 0) 
-    //     { 
-    //         return null; 
-    //     } 
-    //     return goList.ToArray(); 
-    //     }
+    void FixedUpdate()
+    {
+        
+    }
 }
