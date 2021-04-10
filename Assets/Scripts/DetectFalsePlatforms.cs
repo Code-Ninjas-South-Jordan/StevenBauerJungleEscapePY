9b4f42edc2755246672b4979de7c8df8e247fe14
CNSJ <codeninjassouthjordan@gmail.com>
2021-04-10 10:39:35 -0600

    Finished...
    STEPS:
       1. copy c# example of
    https://docs.unity3d.com/ScriptReference/Physics.Raycast.html
       2. remove this line to make it easier to understand.
    layerMask = ~layerMask;
       3. change Math.Infinity in raycast cast to 25f
    NOTES:
    :arrow_forward:
        blocks that are affected are going to be able to be hit with raycast. If facing block and no reaction then it is a safe block.
    
    :arrow_forward:
        you don't need the console the raycast will turn yellow.
    
    :arrow_forward:
        Don't over think it, also make sure you turn on Gizmos to actually see the raycast when you dont have console open, this is very true because console on the bottom right only one and not the current log that is being thrown.
    
    :arrow_forward:
        Did Hit = bad block. Did Not Hit = safe block.
    
    :arrow_forward:
        solution does not identify the one that will disapear after you leave them.

--- Assets/Scripts/DetectFalsePlatforms.cs
+++ Assets/Scripts/DetectFalsePlatforms.cs
@@ -6,19 +6,26 @@ using UnityEngine;
 
 public class DetectFalsePlatforms : MonoBehaviour
 {
-    // private bool RayCast = Physics.Raycast( , Vector3.forward, 10f);
-
-    void Update()
-    {
-        if (Physics.Raycast(transform.position, Vector3.down, 300f, 1 << LayerMask.NameToLayer("RayLayer")))
-        {
-            Debug.Log("I hope this works lol");
-        }
-
-    }
-
+    // See Order of Execution for Event Functions for information on FixedUpdate() and Update() related to physics queries
     void FixedUpdate()
     {
-        
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 25f, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
     }
 }
